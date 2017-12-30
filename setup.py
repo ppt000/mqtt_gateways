34beafa25622cd12bca0da749cea66b6ada2ef7c
ppt000 <paolo.taddonio@empiluma.com>
2017-12-30 01:01:13 +0000

    Update setup files

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     install_requires=['paho-mqtt >= 1.3'],
     package_data={'dummy': ['data/*.map', 'data/*.conf']},
     exclude_package_data={'': ['README.*']},
-    entry_points={'console_scripts': ['dummy2mqtt = mqtt_gateways.dummy.dummy2mqtt:__main__']},
+#    entry_points={'console_scripts': ['dummy2mqtt = mqtt_gateways.dummy.dummy2mqtt:__main__']},
     
     # metadata for upload to PyPI
     author='Pier Paolo Taddonio',
