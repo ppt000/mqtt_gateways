4363825cc6c998d02c4b3bf189f41993b1a363c4
ppt000 <paolo.taddonio@empiluma.com>
2018-05-20 06:13:29 +0100

    split system in different modules

--- mqtt_gateways/musiccast/musiccast_interface.py
+++ mqtt_gateways/musiccast/musiccast_interface.py
@@ -3,9 +3,9 @@
 import json
 
 import mqtt_gateways.musiccast.musiccast_exceptions as mcx
-import mqtt_gateways.musiccast.musiccast_system as mcs
-
+from mqtt_gateways.musiccast.musiccast_system import System
 from mqtt_gateways.musiccast.musiccast_data import ACTIONS
+
 import mqtt_gateways.utils.app_properties as app
 _logger = app.Properties.get_logger(__name__)
 
@@ -13,13 +13,15 @@ class musiccastInterface(object):
     '''The Interface.
 
     Resolves the system definition file path and calls the System class in musiccast_system.
-    Creates the locations dictionary.
+    Creates the locations and devices dictionaries.
 
     Args:
-        params (dictionary): includes whatever options were in the dedicated section
-            of the configuration file.  This class only requires the *sysdefpath* option
-            to be defined as the location of the JSON file describing the system.  If
-            that option is not found the local directory is used instead.
+        params (dictionary): includes all options from the dedicated section
+            of the configuration file.  This class only requires the **sysdefpath** option
+            to be defined. It is the location of the JSON file describing the system.  If
+            that option is not found then the local directory is used instead.
+        msglist_in (list of :class:`internalMsg`): the list of incoming messages.
+        msglist_out (list of :class:`internalMsg`): the list of outgoing messages.
     '''
 
     def __init__(self, params, msglist_in, msglist_out):
@@ -48,29 +50,28 @@ class musiccastInterface(object):
             raise
 
         # instantiate the system structure
-        self._system = mcs.System(json_data, self._msgl_out)
-        # now all the static data is loaded in the structure, but maybe not the 'live' data
+        self._system = System(json_data, self._msgl_out)
 
         # create the location to zone dictionary: key is a location, value is a Zone object
-        self._locations = {zone.location: zone \
-                            for dev in self._system.devices \
-                                for zone in dev.zones if zone.location}
+        self._locations = {zone.location: zone for dev in self._system.devices
+                                                for zone in dev.zones if zone.location}
+
+        # create the device id to device dictionary: key is an id, value is a Device object
+        self._devices = {dev.id: dev for dev in self._system.devices}
 
     def loop(self):
         ''' The method called periodically by the main loop.
 
         The policy to send back status messages depends on the addressing used
-        by the incoming MQTT message: if it is clearly addressed specifically to this
+        by the incoming MQTT message: if it is addressed specifically to this
         interface or to a specific MusicCast device, then a reply will always be sent
         back (case called *assertive*); if it is not, a reply is sent only if a command
-        is executed, otherwise it stays silent as the message is probably intended to
+        is executed, otherwise it stays silent as the message is probably intended for
         somebody else.
         '''
 
-        # process the incoming messages list
-        while True:
-            # read messages on a FIFO basis
-            msg = self._msgl_in.pull()
+        while True: # process the incoming messages list
+            msg = self._msgl_in.pull() # read messages on a FIFO basis
             if msg is None: break # no more messages
 
             _logger.debug(''.join(('Processing message: ', msg.str())))
@@ -80,11 +81,14 @@ class musiccastInterface(object):
             # determine if the message is 'assertive'
             if msg.gateway == 'musiccast': # TODO: there are other cases to deal with
                 assertive = True
-            
+
             # is there a device in the topic?
             if msg.device:
+                if msg.device in self._devices:
+                    assertive = True
+                    # TODO: select the 'default' zone for this device
                 pass
-                
+
             # get the zone for this location
             try: zone = self._locations[msg.location]
             except KeyError: # the location is not found
@@ -93,6 +97,10 @@ class musiccastInterface(object):
                 if assertive: self._msgl_out.push(msg.reply('Error', errtxt))
                 continue # ignore message and go onto next
 
+            # give access to the message attributes by loading them into the system instance
+            self._system.put_msg(msg)
+
+            # TODO: implement self._system.execute_action
             # retrieve the function to execute for this action
             try: func = ACTIONS[msg.action]
             except KeyError: # the action is not found
@@ -101,9 +109,6 @@ class musiccastInterface(object):
                 if assertive: self._msgl_out.push(msg.reply('Error', errtxt))
                 continue # ignore message and go onto next
 
-            # give access to the message attributes by loading them into the system instance
-            self._system.load_msg(msg)
-
             # execute the function in the zone
             try: func(zone)
             except mcx.AnyError as err:
@@ -111,7 +116,7 @@ class musiccastInterface(object):
                 continue
 
             # stack reply on outgoing message list
-            self._msgl_out.push(self.reply())
+            #self._msgl_out.push(self.reply())
 
             #_logger.debug(zone.dump_zone())
 
